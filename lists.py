e500cc5d4a2498340eac26ba59228e1c474dddec
va4oz <va4oz89@gmail.com>
2017-09-02 19:37:21 +0300

    some updates

--- lists.py
+++ lists.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 
-mylist = [1, 'два', 3,1]
-print(len(mylist))
+mylist = [1, 'two', 3,1]
+print(len(mylist)) # число элементов в списке
+print (mylist[0]) # печать первого елемента
+
+mylist.append("new")
+
+print ("added", mylist)
+
+mylist.pop(0)
+
+print ("deleted", mylist)
