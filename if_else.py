71b6b34eec1cce220b3fbacbe65d2e5e4ca1a1df
va4oz <va4oz89@gmail.com>
2017-09-03 00:20:05 +0300

    Test1 was passed

---
+++ if_else.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+a = 12
+if a == 12:
+    print("Верно")
+
+b = 13
+if b == 12:
+    print("верно")
+else:
+    print("не верно")
+
+c = 5
+if c < 2 or c > 3:
+    print("не верно")
+elif c == 2:
+    print("верно")
