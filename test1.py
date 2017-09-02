6a05b16503eff32f1e4f0c89ec592cf4b7036fab
va4oz <va4oz89@gmail.com>
2017-09-02 22:42:41 +0300

    Test1 was passed

---
+++ test1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+mystr = "hello, my dear friend!"
+print(mystr[-7:])
+
+mylist = [1, 2, [18, 45, 87, [7, 6]]]
+a = mylist[2][3]
+print (a[1])
+
+mylist2 = [1, 2, 3, 4, 5, 6, 7]
+mylist2.remove(2)
+print(mylist2)
+
+
+mydic = {"name": "Иван", "surname": "Иванов", "age": 34}
+print (mydic["name"] + " " + mydic["surname"])
+
+
+"""№5 Сделайте из этих двух комментариев
+# один многострочный.
+""
