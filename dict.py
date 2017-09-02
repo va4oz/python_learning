717be30472bdb9f95a9c26a440ffc897507c61db
va4oz <va4oz89@gmail.com>
2017-09-02 19:57:59 +0300

    dict.py was created

---
+++ dict.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+mydict = {1:"first", 2: "second"}
+print(mydict[1]) # печать елемента с индексом 1
+
+superdict = {'name':{'first': 'vlad', 'second': 'Nastia'}, 'dep': ['IT', 'recruting']}
+print (superdict['name']['second'],'--->', superdict['dep'][1])
+
