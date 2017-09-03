8ca414cf44dd857c7b7b1dc611957419c63c7680
va4oz <va4oz89@gmail.com>
2017-09-03 09:19:36 +0300

    for

---
+++ for.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+a = "string"
+for x in a:
+    print(x)
+
+
+b = [1, 2, 3, 4, 5]
+for y in b:
+    print("№2--->", y)
+    for x in range(10):
+        print("№3----->", y)
