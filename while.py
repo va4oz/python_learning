cf6f1fa70b4303d9adb16007a20ec7c77e121dbd
va4oz <va4oz89@gmail.com>
2017-09-03 08:59:44 +0300

    Test1 was passed

---
+++ while.py
@@ -0,0 +1,15 @@
+a = 10
+while a != 0:
+    print(a)
+    a = a -1
+
+b = 10
+while b != 0:
+    b = b - 1
+    if b == 7:
+        pass
+    if b == 5:
+        continue
+    if b == 3:
+        break
+    print (b)
